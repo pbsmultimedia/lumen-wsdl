a4ed75eaca93526824330bccfec9ae2f8fd4714f
Pedro Batista Silva <NB26333@novabase.pt>
2020-04-29 11:23:44 +0100

    indentation

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -18,23 +18,23 @@ class Controller extends BaseController
 		{
 
 			$options = array(
-			            'uri' => "http://$_SERVER[SERVER_ADDR]/wsdl/$service",
-			            'location' => "http://$_SERVER[SERVER_ADDR]/wsdl/$service", // what´s the diff?
-			        );
+				'uri' => "http://$_SERVER[SERVER_ADDR]/wsdl/$service",
+				'location' => "http://$_SERVER[SERVER_ADDR]/wsdl/$service", // what´s the diff?
+			);
 
-	    //$server = new \Laminas\Soap\Server('./docs/test.wsdl', $options); // if the first argument is passed, then the server is on WSDL mode
+			//$server = new \Laminas\Soap\Server('./docs/test.wsdl', $options); // if the first argument is passed, then the server is on WSDL mode
 			$server = new \Laminas\Soap\Server(null, $options);
 
-	    // check if class exists..
-	    if (!class_exists("App\Http\Wsdl\\$service")) {
-					// should return a soap fault..
-	        die("class App\Http\Wsdl\\$service does not exist.");
-	    }
+			// check if class exists..
+			if (!class_exists("App\Http\Wsdl\\$service")) {
+				// should return a soap fault..
+				die("class App\Http\Wsdl\\$service does not exist.");
+			}
 
 			// somewhere it should validate the xml.. using the server in WSDL mode, and comparing the input with the wsdl doc.
 
-	    // Bind class to Soap Server:
-	    $server->setClass("App\Http\Wsdl\\$service");
+			// Bind class to Soap Server:
+			$server->setClass("App\Http\Wsdl\\$service");
 
 			$server->handle();
 
